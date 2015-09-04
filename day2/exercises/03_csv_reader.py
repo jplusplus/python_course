f46a936cbe1982e261af1f4f94f35eebf449235f
rotsee <leo_wallentin@hotmail.com>
2015-09-04 13:50:21 +0200

    style

--- day2/exercises/03_csv_reader.py
+++ day2/exercises/03_csv_reader.py
@@ -5,7 +5,8 @@ import csv
 in_file = open("unemployment.csv", "rb")
 csv_file = csv.DictReader(in_file, delimiter=',')
 
-""" STEG 1: Ta koden från föregående övning, men paketera den som en funktion som
+""" STEG 1: Ta koden från föregående övning,
+    men paketera den som en funktion som
     returnerar en mening om arbetslösheten.
 """
 
@@ -15,9 +16,9 @@ def write_sentence(municipality, unemployment2014, unemployment2013):
     print("En dynamisk mening")
 
 """ STEG 2:
-    Loopa csv-filen och kör funktionen write_sentence() genom att ge den rätt parametrar. 
+    Loopa csv-filen och kör funktionen write_sentence()
+    genom att ge den rätt parametrar.
 """
 for line in csv_file:
     # Skriv kod här!
     print(line)
-
