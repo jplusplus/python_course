56735bfc65a26d082c3e434f79b401af995c775e
rotsee <leo_wallentin@hotmail.com>
2015-09-18 15:41:45 +0200

    more demo

---
+++ teaching-demos/day1/11.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+"""
+ If-satser
+
+"""
+
+population2012 = 9500000
+population2013 = 9600000
+
+if population2013 > population2012:
+    print("Befolkningen växer :)")
+else:
+    print("Vi blir färre :(")
