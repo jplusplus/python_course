bc7da3398e6eaa4a838802b450c08bb601e5d972
rotsee <leo_wallentin@hotmail.com>
2015-09-21 12:51:10 +0200

    extra homework

---
+++ day1/homework/email_writer.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Skriv en funktion som tar ett namn och en domän.
+Gör namnet mejlkompatibelt och skriv ut en fullständig e-postadress.
+"""
+
+
+def emailify(name, domain):
+    """
+     Skriv din kod här!
+    """
+
+emailify("Annie Lööf", "riksdagen.se")
+emailify("David Lång", "riksdagen.se")
+emailify("Emma Nohrén", "riksdagen.se")
+emailify("阿部慎太郎", "asahi.co.jp")
+emailify("Östen Rubin", "dn.se")
