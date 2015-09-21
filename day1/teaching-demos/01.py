ebf725a3dcefa6635df185934d35d99b11631129
rotsee <leo_wallentin@hotmail.com>
2015-09-21 07:58:19 +0200

    rename for easier sorting

---
+++ day1/teaching-demos/01.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+cats = 30
+dogs = 15
+
+if cats > dogs:
+    print("Katter! Katter överallt!")
---
+++ day1/teaching-demos/07.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+"""
+ Slå ihop strängar
+
+"""
+
+first_name = "Jens"
+last_name = "Finnäs"
+
