7c6f8f54ecbf13db4b4d214bb7042d4573af4ec1
jensfinnas <jens.finnas@gmail.com>
2015-09-21 14:45:15 +0200

    Mer utförliga instruktioner

--- day1/homework/email_writer.py
+++ day1/homework/email_writer.py
@@ -7,8 +7,14 @@ Gör namnet mejlkompatibelt och skriv ut en fullständig e-postadress.
 
 def emailify(name, domain):
     """
-     Skriv din kod här!
+     Koden ska göra följande:
+     1) Ersätta mellanslag med punkter i namnet.
+     2) Göra alla tecken till små bokstäver
+     3) Ersätta specialtecken (å,ä,ö,é) med tecken som funkar
+     i en e-postadress.
+     4) Printa den kompletta adressen! 
     """
+    # Skriv din kod här! 
 
 emailify("Annie Lööf", "riksdagen.se")
 emailify("David Lång", "riksdagen.se")
