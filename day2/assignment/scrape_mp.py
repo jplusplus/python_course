1db064a5bc9e375a3521fbf067fa7814306f8cf9
rotsee <leo_wallentin@hotmail.com>
2015-09-24 12:39:13 +0200

    styling

--- day2/assignment/scrape_mp.py
+++ day2/assignment/scrape_mp.py
@@ -1,47 +1,50 @@
 # -*- coding: utf-8 -*-
 
-"""	STEG 1: 
-	Sätt upp en ny virtuell miljö och installera lxml.
-"""
+# STEG 1:
+# Sätt upp en ny virtuell miljö och installera lxml.
 
 import csv
 import urllib2
 import lxml # Eller beautifulsoup?
 
 
-# Den här bjuder vi på 
+# Den här bjuder vi på
 def scrape_profile_page(html):
-	pass
+    pass
 
-""" STEG 2: 
-	Alla ledande MP-politiker har profilsidor med adresser baserat på deras namn.
-	T.ex. http://www.mp.se/om/mats-dahlberg
-	Men vi behöver göra en funktion som skriver om namn ("Mats Dahlberg") till så
-	kallade "slugs" ("mats-dahlberg"), med gemener och utan specialtecken.
-	Använd funktionerna .replace() och .lower() för att göra detta.
-"""
+
 def slugify(name):
-	# Skriv kod här!
-	slug = name
-	return slug
+    # STEG 2
+    """
+     Alla ledande MP-politiker har profilsidor med adresser baserade
+     på deras namn. T.ex. http://www.mp.se/om/mats-dahlberg
+     Den här funktionen skriver om namn ("Mats Dahlberg") till så
+     kallade "slugs" ("mats-dahlberg"), med gemener och utan specialtecken.
+     Använd funktionerna .replace() och .lower() för att göra detta.
+    """
+    # Skriv kod här!
+    slug = name
+    return slug
 
-""" STEG 3: Öppna filen miljopartiet.csv, samt en ny fil som vi kommer att skriva till
-"""
-# Skriv kod här!
 
-"""	STEG 4: 
-	Loopa alla rader i filen miljopartiet.csv med csv.DictReader().
-	Gör följande för varje politiker:
-	- Skapa en url med hjälp av funktionen slugify(), t.ex. http://www.mp.se/om/mats-dahlberg
-	- Öppna urlen läs html-innehållet på sidan
-	- Skicka det här innehållet som en parameter till funktionen scrape_profile_page()
-	- Funktionen kommer att returnera en dictionary som ser ut så här:
-	{
-		"name": "MATS DAHLBERG",
-		"email": "mats.dahlberg@mp.se",
-		"mobile": "0703-98 60 55",
-		"decription": "Ledamot Jokkmokks kommunfullmäktige, ledamot Miljöpartiets partistyrelse"
-	}
-	- Skriv den här informationen till en ny csv-fil
+# STEG 3
+# Öppna filen miljopartiet.csv, samt en ny fil som vi kommer att skriva till
+
+# STEG 4
 """
-# Skriv kod här!
+    Loopa alla rader i filen miljopartiet.csv med csv.DictReader().
+    Gör följande för varje politiker:
+    - Skapa en url med hjälp av funktionen slugify(),
+      t.ex. http://www.mp.se/om/mats-dahlberg
+    - Öppna urlen läs html-innehållet på sidan
+    - Skicka det här innehållet som en parameter
+      till funktionen scrape_profile_page()
+    - Funktionen kommer att returnera en dictionary som ser ut så här:
+    {
+        "name": "MATS DAHLBERG",
+        "email": "mats.dahlberg@mp.se",
+        "mobile": "0703-98 60 55",
+        "description": "Ledamot Jokkmokks kommunfullmäktige, ledamot Miljöpartiets partistyrelse"
+    }
+    - Skriv den här informationen till en ny csv-fil
+"""
