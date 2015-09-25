e4bc3196bd0cdfbc7cc7d5d0f2902e2a9183e479
rotsee <leo_wallentin@hotmail.com>
2015-09-25 14:45:03 +0200

    semicolon sep ssv

--- day2/teaching-demos/6.py
+++ day2/teaching-demos/6.py
@@ -3,7 +3,7 @@
 import csv
 
 in_file = open("unemployment.csv", 'rb')
-rows = csv.DictReader(in_file, delimiter=',')
+rows = csv.DictReader(in_file, delimiter=';')
 
 for row in rows:
-    print(row)
+    print(row['unemployment_2009'])
