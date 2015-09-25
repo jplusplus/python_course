c175f8198b1998880990b00499d9e68ffbe95160
rotsee <leo_wallentin@hotmail.com>
2015-09-25 15:02:12 +0200

    new emailgenerator

--- day2/teaching-demos/joachimmail.py
+++ day2/teaching-demos/joachimmail.py
@@ -6,11 +6,18 @@ Gör namnet mejlkompatibelt och skriv ut en fullständig e-postadress.
 
 
 def emailify(name, domain):
-    name_split = name.split()
-    first_name = name_split[0]
-    last_name = name_split[1]
-    name = first_name + "." + last_name
-    small_name = name.lower()
-    english_name = small_name.replace("ö", "o").replace("ä", "a").replace("å", "a").replace("é", "e").replace("Ö", "o")
+    name_list = name.split(" ")
+    new_name = ".".join(name_list)
+    small_name = new_name.lower()
+    english_name = small_name\
+        .replace("ö", "o")\
+        .replace("ä", "a")\
+        .replace("å", "a")\
+        .replace("ü", "u")\
+        .replace("é", "e")\
+        .replace("É", "e")\
+        .replace("Ö", "o")\
+        .replace("Å", "a")\
+        .replace("Ä", "ä")
     mail_address = english_name + "@" + domain
     print(mail_address)
