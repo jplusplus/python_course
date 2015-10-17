4324cb8e31495a7fd4ef1a69be17edd41d048322
rotsee <leo_wallentin@hotmail.com>
2015-10-17 09:14:58 +0200

    replace_set

---
+++ goodie-bag/replace_set.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+
+def replace_set(text, dictionary):
+    """ Ersätter en uppsättning tecken eller strängar med en annan.
+    """
+    import re
+
+    rep = dict((re.escape(k), v) for k, v in dictionary.iteritems())
+    pattern = re.compile("|".join(rep.keys()))
+    return pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
+
+
+text = replace_set("åbäke", {'å': 'a',
+                             'ä': 'a',
+                             'ö': 'o'
+                             })
+print(text)
+
+text = replace_set("coka cola", {'cola': 'coka',
+                                 'coka': 'cola'})
+print(text)
